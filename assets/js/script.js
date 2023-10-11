efc90aeb334d012aba3622e4bff413f775c7c4e0
Evie Hoang <evie.h0325@gmail.com>
2023-10-10 18:31:13 -0700

    Added variables and missing id for variables. Added placeholder text for correct areas.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,16 @@
 var apiKey = "9c5e768389d1fb88a2f113178aa99b43";
 
 // Variables
+var citySearch = document.getElementById("search");
+var searchBtn = document.getElementById("searchbutton");
+var clearnBtn = document.getElementById("clear");
+
+var cityName = document.getElementById("city");
+var weatherIcon = document.getElementById("weather-icon");
+var nextDays = document.getElementById("upcoming-day");
+var forecast = document.getElementById("forecast");
+var temperature = document.getElementById("temperature");
+var humidity = document.getElementById("humidity");
+var wind = document.getElementById("wind");
+
+// Get Current Weather
